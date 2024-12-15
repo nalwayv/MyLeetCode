8a8a7341c7da1cd763ba7e3c515f0e28a0e884f5
Nick Alway <nalwayv@googlemail.com>
2024-12-15 23:53:13 +0000

    update

--- Leet_509/solution.py
+++ Leet_509/solution.py
@@ -10,19 +10,45 @@ class Solution:
     
     starting from 0 and 1. That is,
     """
-    def fib(self, n: int) -> int:
+    @staticmethod
+    def _calculate_fib(n: int, memo: dict[int, int]) -> int:
+        if n < 2:
+            return n
         
-        def recursive(n: int, cache: dict[int, int]) -> int:
-            if n < 2:
-                return n
-            
-            if n in cache:
-                return cache[n]
+        if n in memo:
+            return memo[n]
         
-            result: int = recursive(n-1, cache) + recursive(n-2, cache)
-            cache[n] = result
-            
-            return result
+        memo[n] = Solution._calculate_fib(n-1, memo) + Solution._calculate_fib(n-2, memo)
+        return memo[n]
         
-        cache: dict[int, int]  = {}
-        return recursive(n, cache)
+
+    def fib_memo(self, n: int) -> int:
+        memo: dict[int, int]  = {}
+        return Solution._calculate_fib(n, memo)
+
+
+    def fib_bottom_up(self, n: int) -> int:
+        if n <= 0:
+            return 0
+        
+        if n == 1:
+            return 1
+        
+        n1: int = 1
+        n2: int = 1
+        
+        for _ in range(2, n):
+            n1, n2 = n2, n1 + n2
+        
+        return n2
+    
+
+def main() -> None:
+    sol = Solution()
+    case1 = sol.fib_memo(10)
+    print(case1)
+    print(f"case1 {"pass" if case1 == 55 else "fail"}")
+
+
+if __name__ == "__main__":
+    main()
