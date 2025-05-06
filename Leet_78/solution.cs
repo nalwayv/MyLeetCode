3cdacfc8d9b04d7be5ce9ebbde06a4028552df8b
Nick Alway <nalwayv@googlemail.com>
2025-05-06 13:04:44 +0100

    update

---
+++ Leet_78/solution.cs
@@ -0,0 +1,52 @@
+namespace Leet78;
+
+public class Solution
+{
+    private readonly IList<int> _subset = [];
+
+    /// <summary>
+    /// Generates all possible subsets (the power set) in an array of integers.
+    /// This method is a recursive helper function that explores all possibilities
+    /// by including or excluding each element.
+    /// </summary>
+    private static void GenerateSubsets(IList<IList<int>> allSubsets, IList<int> currentSubset, int[] numbers, int currentPosition = 0)
+    {
+        if (currentPosition == numbers.Length)
+        {
+            allSubsets.Add(new List<int>(currentSubset));
+            return;
+        }
+
+        // Exclude
+        GenerateSubsets(allSubsets, currentSubset, numbers, currentPosition + 1);
+
+        // Include
+        currentSubset.Add(numbers[currentPosition]);
+        GenerateSubsets(allSubsets, currentSubset, numbers, currentPosition + 1);
+        currentSubset.RemoveAt(currentSubset.Count - 1);
+    }
+    
+    public IList<IList<int>> Subsets(int[] nums)
+    {
+        IList<int> currentSubset = [];
+        IList<IList<int>> allSubsets = [];
+        GenerateSubsets(allSubsets, currentSubset, nums);
+        return allSubsets;
+    }
+}
+
+class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("78. Subsets");
+        Solution solution = new();
+        
+        IList<IList<int>> result = solution.Subsets([ 1, 2, 3 ]);
+        foreach (var subset in result)
+        {
+            Console.WriteLine($"[{string.Join(", ", subset)}]");
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -43,6 +43,7 @@
 - 73\. Set Matrix Zeroes
 - 74\. Search a 2D Matrix
 - 75\. Sort Colors
+- 78\. Subsets
 - 80\. Remove Duplicates from Sorted Array II
 - 82\. Remove Duplicates from Sorted List II
 - 83\. Remove Duplicates from Sorted List
