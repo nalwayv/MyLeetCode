53b457225e6b19036e61d87474a4511351a2c808
Nick Alway <nalwayv@googlemail.com>
2026-01-31 15:00:59 +0000

    update
    changes to leet 744

--- Leet 744/solution.py
+++ Leet 744/solution.py
@@ -1,21 +1,15 @@
 class Solution:
     def nextGreatestLetter(self, letters: list[str], target: str) -> str:
-        target_i: int = ord(target)
-
         lo: int = 0
         hi: int = len(letters) - 1
-
         idx: int = -1
 
         while lo <= hi:
             mid: int = (lo + hi) // 2
-            letter_i: int = ord(letters[mid])
-            diff: int = letter_i - target_i
-
-            if diff <= 0:
-                lo = mid + 1
-            else:
+            if letters[mid] > target:
                 hi = mid - 1
+            else:
+                lo = mid + 1
 
             idx = lo % len(letters)
 
